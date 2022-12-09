18e58318191a70a925c771a69a462963af463f1a
Michael Bombini <bombini.michael@gmail.com>
2022-12-09 20:38:27 +0100

    completed day 1

--- day1/main.py
+++ day1/main.py
@@ -1,17 +1,43 @@
 with open('input.txt') as f:
   text = f.read().strip().split('\n')
 
-# Part One of advent of code
-maxn = 0
-count = 0
+# Part One of day1
+def partOne():
+    maxn = 0
+    count = 0
+    for x in text:
+        if(x == ''):
+            count = 0
+        else:
+            count += int(x)
+        if(count > maxn):
+            maxn=count
+    print(maxn)
+            
+# Part Two of day1
+def partTwo():
+    max1 = 0
+    max2 = 0
+    max3 = 0
+    count = 0
+    for x in text:
+            if(x == ''):
+                count = 0
+            else:
+                count += int(x)
+            if count > max1: 
+                max3 = max2
+                max2 = max1
+                max1 = count
+            elif count > max2:
+                max3 = max2
+                max2 = count    
+            elif count > max3:
+                max3 = count    
+    print(max1+max2+max3)
+    
+partOne()
+partTwo()
 
-for x in text:
-    if(x == ''):
-        count = 0
-    else:
-        count += int(x)
 
-    if(count > maxn):
-        maxn=count
 
-print(maxn)
